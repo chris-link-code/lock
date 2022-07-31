81c7f60d25d6a7a458d52a96370939d21b940aea
chris <chris>
2022-07-31 23:56:04 +0800

    edit

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,12 +1,15 @@
 package com.test;
 
-import com.test.thread.CallableDemo;
+import com.test.thread.GetCatFeture;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        new CallableDemo().callableTest();
+        int processors = Runtime.getRuntime().availableProcessors();
+        log.info("processors: " + processors);
+
+        // new CallableDemo().callableTest();
 
         //new CountDownLatchDemo().countDownLatchTest();
 
@@ -19,6 +22,10 @@ public class Main {
         //new FutureDemo().futureTest();
 
         //log.info(Convert.zConvert("PAYPALISHIRING", 3));
+
+        //new GetCat().getAge();
+
+        new GetCatFeture().getAge();
 
         log.info("end main()");
     }
---
+++ src/main/java/com/test/thread/GetCatFeture.java
@@ -0,0 +1,46 @@
+package com.test.thread;
+
+import com.test.bean.Cat;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+/**
+ * @author chris
+ * @create 2022/7/31
+ * <p>
+ * 多线程案例
+ */
+@Slf4j
+public class GetCatFeture {
+    public void getAge() {
+        long start = System.currentTimeMillis();
+        List<Cat> list = Arrays.asList(
+                new Cat("Tom"),
+                new Cat("Jerry"),
+                new Cat("Lily"),
+                new Cat("Brown"),
+                new Cat("Cherry"),
+                new Cat("Alen"),
+                new Cat("Frank"),
+                new Cat("Kate")
+        );
+
+        List<String> collect = list.stream().map(cat ->
+                        CompletableFuture.supplyAsync(() ->
+                                String.format("cat %s age is %d", cat.getName(), cat.getAge())))
+                .collect(Collectors.toList())
+                .stream()
+                .map(s -> s.join())
+                .collect(Collectors.toList());
+
+        for (String item : collect) {
+            log.info(item);
+        }
+        long end = System.currentTimeMillis();
+        log.info("spend time " + (end - start));
+    }
+}
