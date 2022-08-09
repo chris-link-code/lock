3c92cd63cf7d11b90c2597597a67b4f78a1f4a4b
chris <chris>
2022-08-10 00:08:04 +0800

    edit

--- src/main/java/com/test/thread/CompletableFutureDemo.java
+++ src/main/java/com/test/thread/CompletableFutureDemo.java
@@ -19,7 +19,8 @@ public class CompletableFutureDemo {
             log.info("processors: " + processors);
             executor = Executors.newFixedThreadPool(processors);
 
-            completable(executor);
+            //supply(executor);
+            handle(executor);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         } finally {
@@ -30,10 +31,16 @@ public class CompletableFutureDemo {
         }
     }
 
-    public void completable(Executor executor) {
+    /**
+     * 多个步骤都是由同一线程执行
+     * 若其中一步报错，则程序终止
+     * @param executor 线程池
+     */
+    public void supply(Executor executor) {
+        long start = System.currentTimeMillis();
         CompletableFuture.supplyAsync(() -> {
             try {
-                TimeUnit.MILLISECONDS.sleep(10);
+                TimeUnit.MILLISECONDS.sleep(1000);
             } catch (InterruptedException e) {
                 log.error(e.getMessage(), e);
             }
@@ -41,15 +48,16 @@ public class CompletableFutureDemo {
             return 1;
         }, executor).thenApply(v -> {
             try {
-                TimeUnit.MILLISECONDS.sleep(10);
+                TimeUnit.MILLISECONDS.sleep(1000);
             } catch (InterruptedException e) {
                 log.error(e.getMessage(), e);
             }
+            int i = 1 / 0;
             log.info("step 2");
             return v + 2;
         }).thenApply(v -> {
             try {
-                TimeUnit.MILLISECONDS.sleep(10);
+                TimeUnit.MILLISECONDS.sleep(1000);
             } catch (InterruptedException e) {
                 log.error(e.getMessage(), e);
             }
@@ -57,13 +65,57 @@ public class CompletableFutureDemo {
             return v + 3;
         }).whenComplete((v, e) -> {
             if (e == null) {
-                log.info("complete\t" + v);
+                long end = System.currentTimeMillis();
+                log.info("spend time " + (end - start) + "ms complete\t" + v);
             }
         }).exceptionally(e -> {
             log.error(e.getMessage(), e);
             return null;
         });
+        log.info(Thread.currentThread().getName() + " is running");
+    }
 
+    /**
+     * 多个步骤都是由同一线程执行
+     * 若其中一步报错，剩下的步骤依然可以继续执行
+     * @param executor 线程池
+     */
+    public void handle(Executor executor) {
+        long start = System.currentTimeMillis();
+        CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (InterruptedException e) {
+                log.error(e.getMessage(), e);
+            }
+            log.info("step 1");
+            return 1;
+        }, executor).handle((v,e) -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (InterruptedException ex) {
+                log.error(e.getMessage(), e);
+            }
+            int i = 1 / 0;
+            log.info("step 2");
+            return v + 2;
+        }).handle((v,e) -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (InterruptedException ex) {
+                log.error(e.getMessage(), e);
+            }
+            log.info("step 3");
+            return v + 3;
+        }).whenComplete((v, e) -> {
+            if (e == null) {
+                long end = System.currentTimeMillis();
+                log.info("spend time " + (end - start) + "ms complete\t" + v);
+            }
+        }).exceptionally(e -> {
+            log.error(e.getMessage(), e);
+            return null;
+        });
         log.info(Thread.currentThread().getName() + " is running");
     }
 }
