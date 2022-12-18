0a36b4a470fc41691b3b2cc93b126ef7794ac589
links <links@yqsoftware.com.cn>
2022-12-18 22:58:39 +0800

    edit

--- src/main/java/com/test/thread/VolatileDemo.java
+++ src/main/java/com/test/thread/VolatileDemo.java
@@ -2,6 +2,8 @@ package com.test.thread;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author chris
  * @create 2022/12/17
@@ -12,15 +14,25 @@ public class VolatileDemo {
 
     public void volatileTest() {
         new Thread(() -> {
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < 4; i++) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage(), e);
+                }
                 count--;
                 log.info("count: {}", count);
             }
         }, "T_1").start();
 
         new Thread(() -> {
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < 4; i++) {
                 count--;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage(), e);
+                }
                 log.info("count: {}", count);
             }
         }, "T_2").start();
