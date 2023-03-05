406c45a370ccc975a2d4ebcb8e407035feda2638
chris <chris>
2023-03-05 21:47:07 +0800

    edit

--- src/main/java/com/demo/jvm/ObjectDemo.java
+++ src/main/java/com/demo/jvm/ObjectDemo.java
@@ -15,20 +15,20 @@ public class ObjectDemo {
      * Java对象的内存结构
      * Java对象的内存结构分为对象头、对象体和对齐字节。
      * <p>
-     * 1.对象头
-     * 1.1标记字
+     * 1.对象头(header)
+     * 1.1标记字(mark word)
      * 用来存储自身运行时的数据，例如：对象的分代年龄、hashCode、锁状态等信息。
      * <p>
-     * 1.2类对象指针
+     * 1.2类型指针(class pointer)
      * 用来存储方法区中字节码对象的地址，JVM通过这个指针来确定这个对象是属于那个类的实例。
      * <p>
-     * 1.3数组长度
+     * 1.3数组长度(length)
      * 如果对象是数组，则该字段记录数组的长度，如果不是数组，该字段不存在。是一个可选字段。
      * <p>
-     * 2.对象体
+     * 2.对象体(instance data)
      * 对象的成员属性，也包括父类的成员属性。
      * <p>
-     * 3.对齐字节
+     * 3.对齐字节(padding)
      * 对齐字节也叫作填充对齐，其作用是用来保证Java对象所占内存字节数为8的倍数，
      * HotSpot VM的内存管理要求对象起始地址必须是8字节的整数倍
      * <p>
@@ -45,11 +45,24 @@ public class ObjectDemo {
      * VALUE：对应内存中存储的值;
      */
     public void test() {
-        Object object = new Object();
-        log.info("object memory space:\r\n{}", ClassLayout.parseInstance(object).toPrintable());
-
         Cat cat = new Cat("tom");
         User user = new User(3, "Jerry", cat);
         log.info("user memory space:\r\n{}", ClassLayout.parseInstance(user).toPrintable());
+
+        Object object = new Object();
+        log.info("object memory space:\r\n{}", ClassLayout.parseInstance(object).toPrintable());
+
+        synchronized (object) {
+            log.info("synchronized object memory space:\r\n{}", ClassLayout.parseInstance(object).toPrintable());
+        }
+
+        Thread thread = new Thread(() -> {
+            synchronized (object) {
+                log.info("thread synchronized object memory space:\r\n{}", ClassLayout.parseInstance(object).toPrintable());
+            }
+        }, "t_1");
+
+        thread.start();
+        log.info("thread start object memory space:\r\n{}", ClassLayout.parseInstance(object).toPrintable());
     }
 }
