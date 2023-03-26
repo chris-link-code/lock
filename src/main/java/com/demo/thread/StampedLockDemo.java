3a37147c95a7323a0f620fd7e7aef69c7a895684
chris <chris>
2023-03-26 11:33:29 +0800

    StampedLockDemo

--- src/main/java/com/demo/thread/StampedLockDemo.java
+++ src/main/java/com/demo/thread/StampedLockDemo.java
@@ -9,7 +9,7 @@ import java.util.concurrent.locks.StampedLock;
  * @author chris
  * @create 2023/3/25
  * https://perfectcode.top/2022/06/10/ReentrantLock%E3%80%81ReentrantReadWriteLock%E3%80%81StampedLock%E8%AE%B2%E8%A7%A3/
- * StampedLock = ReentrantReadWriteLock + 读的过程中也允许获取写锁介入
+ * StampedLock = ReentrantReadWriteLock + 读的过程中也允许写锁介入
  */
 @Slf4j
 public class StampedLockDemo {
@@ -20,17 +20,10 @@ public class StampedLockDemo {
     public void write() {
         long stamp = stampedLock.writeLock();
         try {
-            log.info("{} start write, number: {}, stamp: {}",
-                    Thread.currentThread().getName(), number, stamp);
-            try {
-                TimeUnit.MILLISECONDS.sleep(200);
-            } catch (InterruptedException e) {
-                log.error(e.getMessage(), e);
-            }
+            log.info("start write, number: {}, stamp: {}", number, stamp);
             number = number + 1;
         } finally {
-            log.info("{} write end, number: {}, stamp: {}",
-                    Thread.currentThread().getName(), number, stamp);
+            log.info("write end, number: {}, stamp: {}", number, stamp);
             stampedLock.unlockWrite(stamp);
         }
     }
@@ -54,28 +47,26 @@ public class StampedLockDemo {
 
     public void tryOptimisticRead() {
         long stamp = stampedLock.tryOptimisticRead();
-
-        /*try {
-            TimeUnit.MILLISECONDS.sleep(100);
+        log.info("tryOptimisticRead start, number: {}, stamp: {}", number, stamp);
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
         } catch (InterruptedException e) {
             log.error(e.getMessage(), e);
-        }*/
+        }
 
+        // stampedLock.validate(stamp): true无修改，false有修改
         if (stampedLock.validate(stamp)) {
-            log.info("{} tryOptimisticRead, number: {}, stamp: {}, validate(stamp): {} (true无修改，false有修改)",
-                    Thread.currentThread().getName(), number, stamp, stampedLock.validate(stamp));
+            log.info("tryOptimisticRead, number: {}, stamp: {}, ", number, stamp);
         } else {
             log.info("有人修改过，有写操作，从乐观读升级为悲观读，并重新获取数据");
             stamp = stampedLock.readLock();
             try {
-                log.info("{} tryOptimisticRead, number: {}, stamp: {}, validate(stamp): {} (true无修改，false有修改)",
-                        Thread.currentThread().getName(), number, stamp, stampedLock.validate(stamp));
+                log.info("reread number: {}, stamp: {}", number, stamp);
             } finally {
                 stampedLock.unlockRead(stamp);
             }
         }
-        log.info("{} tryOptimisticRead end, number: {}, stamp: {}",
-                Thread.currentThread().getName(), number, stamp);
+        log.info("tryOptimisticRead end, number: {}, stamp: {}", number, stamp);
     }
 
     //乐观读，读的过程中也允许获取写锁介入
@@ -112,7 +103,7 @@ public class StampedLockDemo {
         //new Thread(() -> read(), "read_thread").start();
         //new Thread(() -> tryOptimisticRead(), "optimistic_read_thread").start();
         for (int i = 0; i < LOOP_SIZE; i++) {
-            new Thread(() -> tryOptimisticRead(), "optimistic_read_thread").start();
+            new Thread(() -> tryOptimisticRead(), "optimistic_read_thread_" + i).start();
         }
         new Thread(() -> write(), "write_thread").start();
     }
